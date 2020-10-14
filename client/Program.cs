c3b54ec7d04c68b3885eeb04254559f6d05f6165
gklittlejohn <gklittlejohn@googlemail.com>
2020-10-14 13:15:10 +0100

    basic example of issue

---
+++ client/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                 .WithUrl("https://localhost:5001/chathub", options =>
+                 {
+                    options.UseDefaultCredentials = true;
+                    options.Transports = Microsoft.AspNetCore.Http.Connections.HttpTransportType.WebSockets;
+                 })
+                 .ConfigureLogging(logging => logging.AddConsole())
+                 .WithAutomaticReconnect()
+                 .Build();
+            try
+            {
+                await connection.StartAsync();
+
+                Console.WriteLine("Starting connection. Press Ctrl-C to close.");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (sender, a) =>
+                {
+                    a.Cancel = true;
+                    Console.WriteLine("Stopping loops...");
+                    cts.Cancel();
+                };
+
+                connection.Closed += e =>
+                {
+                    Console.WriteLine("Connection closed with error: {0}", e);
+
+                    cts.Cancel();
+                    return Task.CompletedTask;
+                };
+
+                // keep the thread alive
+                while (!cts.IsCancellationRequested)
+                    await Task.Delay(1000);
+            }
+            catch (AggregateException aex) when (aex.InnerExceptions.All(e => e is OperationCanceledException))
+            {
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            finally
+            {
+                await connection.DisposeAsync();
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ server/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chathub");
+            });
+        }
+    }
+
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
